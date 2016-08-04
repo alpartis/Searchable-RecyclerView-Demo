7b8aef275ae0e5ee0b7fb39b8d0b1f71d8bd8eaa
alpartis <alpartis@thundernet.com>
2016-08-04 00:38:56 -0400

    Revert "Revert "add expandable space in list item""
    This reverts commit f9027915270334ab8471ea790a610aa236ffba37.

--- app/src/main/java/com/github/wrdlbrnft/searchablerecyclerviewdemo/ui/adapter/viewholder/ExampleViewHolder.java
+++ app/src/main/java/com/github/wrdlbrnft/searchablerecyclerviewdemo/ui/adapter/viewholder/ExampleViewHolder.java
@@ -1,23 +1,43 @@
 package com.github.wrdlbrnft.searchablerecyclerviewdemo.ui.adapter.viewholder;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Space;
 import android.widget.TextView;
 
 import com.github.wrdlbrnft.searchablerecyclerviewdemo.R;
 import com.github.wrdlbrnft.searchablerecyclerviewdemo.ui.adapter.models.ExampleModel;
+import com.github.wrdlbrnft.searchablerecyclerviewdemo.ui.fragments.MainFragment;
 
-public class ExampleViewHolder extends RecyclerView.ViewHolder {
 
-    private final TextView tvText;
+
+public class ExampleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    private final String    LOGTAG = ExampleViewHolder.class.getSimpleName();
+    private final View      _myView;
+    private final TextView  _textView;
+    private final Space     _spaceView;
 
     public ExampleViewHolder(View itemView) {
         super(itemView);
-
-        tvText = (TextView) itemView.findViewById(R.id.tvText);
+        _myView = itemView;
+        _textView = (TextView) itemView.findViewById(R.id.tvText);
+        _textView.setOnClickListener(this);
+        _spaceView = (Space) itemView.findViewById(R.id.space);
     }
 
     public void bind(ExampleModel model) {
-        tvText.setText(model.getText());
+        _textView.setText(model.getText());
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        Log.d(LOGTAG, "onClick(): user touched me at position: " + Integer.toString(getAdapterPosition()));
+        if (v.getId() == R.id.tvText) {
+            _spaceView.setVisibility(View.VISIBLE);
+            ((MainFragment)_myView.getTag()).scrollTo(getAdapterPosition());
+        }
     }
 }
--- app/src/main/java/com/github/wrdlbrnft/searchablerecyclerviewdemo/ui/fragments/MainFragment.java
+++ app/src/main/java/com/github/wrdlbrnft/searchablerecyclerviewdemo/ui/fragments/MainFragment.java
@@ -20,8 +20,8 @@ import com.github.wrdlbrnft.searchablerecyclerviewdemo.ui.adapter.models.Example
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainFragment extends Fragment implements SearchView.OnQueryTextListener {
-
+public class MainFragment extends Fragment implements SearchView.OnQueryTextListener
+{
     public static MainFragment newInstance() {
         return new MainFragment();
     }
@@ -81,16 +81,15 @@ public class MainFragment extends Fragment implements SearchView.OnQueryTextList
             "Home"
     };
 
-    private RecyclerView mRecyclerView;
-    private ExampleAdapter mAdapter;
-    private List<ExampleModel> mModels;
+    private RecyclerView        _recyclerView;
+    private ExampleAdapter      _adapter;
+    private List<ExampleModel>  _models;
+    private LinearLayoutManager _linearLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-
+        _recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         return view;
     }
 
@@ -99,16 +98,17 @@ public class MainFragment extends Fragment implements SearchView.OnQueryTextList
         super.onViewCreated(view, savedInstanceState);
         setHasOptionsMenu(true);
 
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        _linearLayoutManager = new LinearLayoutManager(getActivity());
+        _recyclerView.setLayoutManager(_linearLayoutManager);
 
-        mModels = new ArrayList<>();
+        _models = new ArrayList<>();
 
         for (String movie : MOVIES) {
-            mModels.add(new ExampleModel(movie));
+            _models.add(new ExampleModel(movie));
         }
 
-        mAdapter = new ExampleAdapter(getActivity(), mModels);
-        mRecyclerView.setAdapter(mAdapter);
+        _adapter = new ExampleAdapter(getActivity(), this, _models);
+        _recyclerView.setAdapter(_adapter);
     }
 
     @Override
@@ -122,9 +122,9 @@ public class MainFragment extends Fragment implements SearchView.OnQueryTextList
 
     @Override
     public boolean onQueryTextChange(String query) {
-        final List<ExampleModel> filteredModelList = filter(mModels, query);
-        mAdapter.animateTo(filteredModelList);
-        mRecyclerView.scrollToPosition(0);
+        final List<ExampleModel> filteredModelList = filter(_models, query);
+        _adapter.animateTo(filteredModelList);
+        _recyclerView.scrollToPosition(0);
         return true;
     }
 
@@ -145,8 +145,9 @@ public class MainFragment extends Fragment implements SearchView.OnQueryTextList
         }
         return filteredModelList;
     }
+
     public void scrollTo(int position)
     {
-        mRecyclerView.scrollToPosition(position);
+        _linearLayoutManager.scrollToPosition(position);
     }
 }
